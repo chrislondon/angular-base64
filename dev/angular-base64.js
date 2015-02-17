0c132b71e3b93400170e62cad6b7d5b2d42f150f
Chris London <me@chrislondon.co>
2015-02-16 21:57:14 -0700

    fix Base64 reference

--- dev/angular-base64.js
+++ dev/angular-base64.js
@@ -19,7 +19,7 @@ angular.module('ngBase64', [])
                 var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
                 var i = 0;
 
-                input = Base64._utf8_encode(input);
+                input = factory._utf8_encode(input);
 
                 while (i < input.length) {
 
@@ -78,7 +78,7 @@ angular.module('ngBase64', [])
 
                 }
 
-                output = Base64._utf8_decode(output);
+                output = factory._utf8_decode(output);
 
                 return output;
 
